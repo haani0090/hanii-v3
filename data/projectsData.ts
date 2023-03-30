3eace142eb02fae5bf26d34031b4c03baa86dd9c
Rehan Aslam <83347128+haani0090@users.noreply.github.com>
2023-03-30 12:14:24 +0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,14 +9,14 @@ const projectsData = [
   {
     title: 'Slicking',
     description: `Slicking helps you work the retail system so that you can be comfortable, look sharp, and save money.`,
-    imgSrc: '/static/images/project/slickin.png',
+    imgSrc: '/static/slickin .png',
     tag: ['typescript', 'nextjs', 'tailwindcss','notion'],
     href: 'https://slicking.vercel.app/',
   },
   {
     title: 'Rentoor',
     description: ` Enhancing old photos Using AI for everyone.`,
-    imgSrc: '/static/images/project/rentoor.png',
+    imgSrc: '/static/rentoor.png',
     tag: ['typescript', 'nextjs','replicate'],
     href: 'https://rentoor.vercel.app/',
   } 
